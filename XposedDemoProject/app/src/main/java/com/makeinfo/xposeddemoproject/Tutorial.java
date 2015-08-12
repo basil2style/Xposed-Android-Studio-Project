0f24af1464532141a54f77333763ffdb42e37cbd
Basil <basil2style@gmail.com>
2015-08-12 16:01:49 +0530

    xposed demo code

---
+++ XposedDemoProject/app/src/main/java/com/makeinfo/xposeddemoproject/Tutorial.java
@@ -0,0 +1,15 @@
+package com.makeinfo.xposeddemoproject;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by Basil on 8/12/2015.
+ */
+public class Tutorial implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        XposedBridge.log("Testing Template :) .....");
+    }
+}
